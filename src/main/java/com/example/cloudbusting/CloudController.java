444a691844039c2ee9334802883b63d67fed85bf
Patricia Edwards <patriciaje16@gmail.com>
2022-05-30 11:36:38 +0100

    first commit

---
+++ src/main/java/com/example/cloudbusting/CloudController.java
@@ -0,0 +1,17 @@
+package com.example.cloudbusting;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CloudController {
+
+
+    //create a get request method that returns a string
+    @GetMapping("/message")
+    public String getMessage(){
+        return "hello and welcome to cloud";
+    }
+
+}
